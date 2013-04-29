718366a3468111ebefd97e59a310012c45ebf6be
Zhaofeng Zhou <neaghfoz@gmail.com>
2013-04-30 00:45:47 +0800

    添加后台管理模板charisma并测试登录界面

---
+++ web/src/main/java/com/neaghfoz/webapp/action/User.java
@@ -0,0 +1,15 @@
+package com.neaghfoz.webapp.action;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neaghfoz
+ * Date: 13-4-30
+ * Time: 上午12:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class User extends BaseAction {
+
+    public String toAdmin() {
+        return SUCCESS;
+    }
+}
