f3ab193f8b638557acc18b0fcedd66d0111d857c
zhong <btxhuman@gmail.com>
2013-05-02 23:22:11 +0800

    完善demo,添加使用事务的形式（可以通过注解或者xml配置来使用事务）、添加aop(切面增强处理)使用例子（暂时只写了在xml下配置aop的例子，通过注解@Aspect 来自动注入使用aop这种方式不算写例子。）

--- core/src/main/java/com/neaghfoz/component/demo/demo1/service/impl/Demo1Service.java
+++ core/src/main/java/com/neaghfoz/component/demo/demo1/service/impl/Demo1Service.java
@@ -8,24 +8,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.neaghfoz.component.demo.demo1.service.IDemo1Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
 public class Demo1Service implements IDemo1Service {
 
-	private static final Logger log  = LoggerFactory.getLogger(Demo1Service.class);
-	
-	@Resource
-	private IDemo1DAO demo1DAO;
-	
-	@Override
-	public void saveDemo1() throws SQLException{
-		log.debug("测试日志");
+    private static final Logger log = LoggerFactory.getLogger(Demo1Service.class);
+
+    @Resource
+    private IDemo1DAO demo1DAO;
+
+    @Override
+    /*
+        使用@Transactional 这个注解的话，就可以不用在applicationontext.xml配置文件里面配置aop:config 这个配置项
+    */
+/*
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {SQLException.class})
+*/
+    public void saveDemo1() throws SQLException {
+        log.debug("测试日志");
         Demo1PO demo1PO = new Demo1PO();
         demo1PO.setName("hello world");
         demo1DAO.addDemo1(demo1PO);
+        //可以注释掉这个异常
+/*
         throw new SQLException("用来测试事务是否开启");
-	}
+*/
+    }
 
 }
-	
