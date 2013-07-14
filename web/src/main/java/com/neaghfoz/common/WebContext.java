f35fec056aa6b367c6acb4db3896fd3d2b159777
btxhuman <btxhuman@gmail.com>
2013-07-15 06:59:36 +0800

    修改部分配置以及完善登录菜单。

---
+++ web/src/main/java/com/neaghfoz/common/WebContext.java
@@ -0,0 +1,11 @@
+package com.neaghfoz.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mr-Zhong
+ * Date: 13-7-13
+ * Time: 下午11:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebContext {
+}
---
+++ web/src/main/java/com/neaghfoz/framework/springsecurity/LoginSuccessHandler.java
@@ -0,0 +1,11 @@
+package com.neaghfoz.framework.springsecurity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mr-Zhong
+ * Date: 13-7-13
+ * Time: 下午11:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginSuccessHandler {
+}
